c9f874eb18a09f103d0170e28f15e57efb2b5875
Dang Duc Hieu <ddhieu3103@gmail.com>
2023-05-21 15:51:32 +0700

    ôn tập hàm+hoán đổi giá trị hai số

---
+++ Baitapmangnangcao/bai1.py
@@ -0,0 +1,5 @@
+#cho mang diem hoc sinh [8,7,8,7,6,4,5,8,9]
+#1. tim so diem lon nhat
+#2. tim so diem nho nhat
+#3. tim xem co bao nhieu diem 8
+
---
+++ Baitapmangnangcao/doigiatricua2so.py
@@ -0,0 +1,53 @@
+# cho 2 so a=10, b=15
+# viet ct doi gia tri cua 2 so tren
+# sau khi doi thi a=15, b=10
+
+
+
+
+
+#yeu cau: dong goi thanh ham
+def hamdoigiatri(a,b):
+    print("da chay vo ham")
+    c=a #c=20
+    a=b #a=15
+    b=c #b=20
+    print("da chay xong ham")
+# ket thuc ham
+
+
+sothunhat=100
+sothuhai=500
+
+print("so thu nhat truoc khi hoan doi gia tri la:",sothunhat)
+print("so thu hai truoc khi hoan doi gia tri la:",sothuhai)
+
+hamdoigiatri(sothunhat,sothuhai)
+
+print("so thu nhat sau khi hoan doi gia tri la:",sothunhat)
+print("so thu hai sau khi hoan doi gia tri la:",sothuhai)
+
+
+# nhan
+# def inraten(ten):
+    # print("ten cua toi nhan duoc la", ten)
+# inraten("Chut")
+
+def tinhtonghaiso(so1, so2):
+    tong=so1/so2
+    print("tong cua hai so la:", tong)
+
+
+
+sothunhut=100
+sothuhai=500
+tinhtonghaiso(sothunhut,sothuhai)
+
+
+
+
+
+    
+    
+
+
